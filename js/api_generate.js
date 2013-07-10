e9cfa73ff5d0a342490292c810e60abfe8780119
dstrek <xathynz@gmail.com>
2013-07-10 15:39:06 +1000

    no caching on request

--- js/api_generate.js
+++ js/api_generate.js
@@ -5,8 +5,9 @@ $(document).ready(function () {
 	var category = index_div.data('category');
 
 	var api_div = $("#api-all");
+	var time = (new Date()).getTime();
 
-	$.getJSON("../../json/" + product + "/" + category + ".json", function(data) {
+	$.getJSON("../../json/" + product + "/" + category + ".json" + "?_=" + time, function(data) {
 		generate_api(data);
 		//generate_api(JSON.parse(data));
 	});
@@ -23,11 +24,11 @@ $(document).ready(function () {
 
 		*/
 
-		var endpoints = data['endpoints'];
+		var endpoints = data.endpoints;
 		for (var i = 0; i < endpoints.length; i++) {
 
 			ep = endpoints[i];
-			safe_name = ep.name.replace(/ /g,'-')
+			safe_name = ep.name.replace(/ /g,'-');
 			str = '';
 			str += '<div class="index-item-wrapper">';
 			str += '<a href="#'+safe_name+'"><span class="index-item">'+ep.name+'</span></a><br />';
@@ -40,12 +41,12 @@ $(document).ready(function () {
 			str += '<code class="endpoint">'+ep.method + " " + ep.url+'</code>';
 			str += '<h6>Description</h6><p class="detail">'+ep.description+'</p>';
 			str += '<h6>Parameters</h6><p class="detail">';
-			if (ep.parameters != null) {
+			if ( ! ep.parameters) {
 				str += '<table class="parameters">';
 				for (var j = 0; j < ep.parameters.length; j++) {
 					str += '<tr><td><code>'+ep.parameters[j].name+'</code></td><td>'+ep.parameters[j].description+'</td></tr>';
 				}
-				str += '</table>'
+				str += '</table>';
 			} else {
 				str += '(none)';
 			}
@@ -53,7 +54,7 @@ $(document).ready(function () {
 
 			str += '<div class="code-header request"><h4>Example Request Body</h4></div>';
 			str += '<pre class="json-block prettyprint linenums">';
-			if (ep.examples.request != null) {
+			if ( ! ep.examples.request) {
 				str += vkbeautify.json(JSON.stringify(ep.examples.request));
 			} else {
 				str += '(none)';
@@ -61,14 +62,14 @@ $(document).ready(function () {
 			str += '</pre>';
 			str += '<div class="code-header response"><h4>Example Response Body</h4></div>';
 			str += '<pre class="json-block prettyprint linenums">';
-			if (ep.examples.response != null) {
+			if ( ! ep.examples.response) {
 				str += vkbeautify.json(JSON.stringify(ep.examples.response));
 			} else {
 				str += '(none)';
 			}
 			str += '</pre>';
 			str += '</div>';
-			str += '<br />'
+			str += '<br />';
 			api_div.append(str);
 
 		}
